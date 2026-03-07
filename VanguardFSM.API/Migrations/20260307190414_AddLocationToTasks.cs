f90d39585dd5c658194fa4f7b4669c08ad0a73b3
shamilsurawweera <shamilsurawweera@gmail.com>
2026-03-08 00:35:32 +0530

    feat: enable SQL Server Spatial Data for Smart Dispatch
    - Installed Microsoft.EntityFrameworkCore.SqlServer.NetTopologySuite.
    - Configured AppDbContext to use NetTopologySuite for geographic types.
    - Added Point (GPS) location property to TaskItem model.
    - Applied migration to update SQL Server schema with geography support.

---
+++ VanguardFSM.API/Migrations/20260307190414_AddLocationToTasks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace VanguardFSM.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocationToTasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Point>(
+                name: "Location",
+                table: "Tasks",
+                type: "geography",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Tasks");
+        }
+    }
+}
--- VanguardFSM.API/Program.cs
+++ VanguardFSM.API/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    x => x.UseNetTopologySuite()));
 
 var app = builder.Build();
 
